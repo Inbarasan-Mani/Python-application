f5c3e8cd4d96016c886ee4dfd8c1fb1647f7a7a8
Inbarasan Manivannan <inbarasanmanivannan@outlook.com>
2022-11-29 12:02:30 +0530

    Initial commit

---
+++ application.py
@@ -0,0 +1,11 @@
+# Application file contains of functions:
+
+def addition(a, b):
+	return f'The sum of two numbers {a} and {b} is {a+b}'
+
+
+def upper(string):
+	return f'Uppercase of the string - {string}: {string.upper()}'
+
+def lower(string):
+	return f'Lowercase of the string - {string}: {string.lower()}'
---
+++ main.py
@@ -0,0 +1,10 @@
+from application import upper, lower, addition
+
+up = upper("Jenkins")
+print(up)
+
+low = lower("Inbarasan MANIVANNAN")
+print(low)
+
+add = addition(12, 678)
+print(add)
